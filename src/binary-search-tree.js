964cae8fda15ead526165e06c9e5e1bb0621936a
svereschako <33933400+svereschako@users.noreply.github.com>
2025-11-13 04:16:39 +0200

    fix: amend function

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,155 +1,192 @@
-const { NotImplementedError } = require("../extensions/index.js");
+const { NotImplementedError } = require('../extensions/index.js');
 
-const { Node } = require("../extensions/list-tree.js");
+ const { Node } = require('../extensions/list-tree.js');
 
 /**
- * Implement simple binary search tree according to task description
- * using Node from extensions
- */
+* Implement simple binary search tree according to task description
+* using Node from extensions
+*/
 class BinarySearchTree {
+
   constructor() {
     this._root = null;
   }
 
   root() {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here    
     return this._root;
   }
 
-  // helper method which creates a new node to
-  // be inserted and calls insertNode
-  add(data) {
-    // Creating a node and initialising with data
-    let newNode = new Node(data);
+  add( data ) {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    var node = new Node(data);
 
-    // root is null then node will be added to the tree and made root.
-    if (this._root === null) this._root = newNode;
-    // find the correct position in the tree and add the node
-    else this.insertNode(this._root, newNode);
+    if (this._root==null) {
+      this._root = node;
+      return;
+    }
+    let currentChildren = this._root;
+    this.findNodeToInsert(data, currentChildren);
+    /*if (data < current.data && current.left) {
+      current.left.add(data);
+    } else if (data < current.data) {
+      current.left = new Node(data);
+    }
+
+    if (data > current.data && current.right) {
+      current.right.add(data);
+    } else if (data > current.data) {
+      current.right = new Node(data);
+    }*/
+    
+  
   }
 
-  // Method to insert a node in a tree. It moves over the tree to find the location to insert a node with a given data
-  insertNode(node, newNode) {
-    // if the data is less than the node data move left of the tree
-    if (newNode.data < node.data) {
-      // if left is null insert node here
-      if (node.left === null) node.left = newNode;
-      // if left is not null recur until null is found
-      else this.insertNode(node.left, newNode);
+  findNodeToInsert(data, parentNode) {
+    if (data > parentNode.data) {
+        if (parentNode.right) {
+            this.findNodeToInsert(data, parentNode.right);
+            return;
+        }
+        parentNode.right = new Node(data);
     }
-
-    // if the data is more than the node data move right of the tree
-    else {
-      // if right is null insert node here
-      if (node.right === null) node.right = newNode;
-      // if right is not null recur until null is found
-      else this.insertNode(node.right, newNode);
-    }
-  }
-
-  has(data) {
-    return this._searchNode(this._root, data) !== null;
-  }
-
-  find(data) {
-    return this._searchNode(this._root, data);
-  }
-
-  _searchNode(node, data) {
-    // base case (we have't found anything)
-    if (!node) {
-      return null;
-    }
-
-    // if data matches the current node => we've found the node
-    if (data === node.data) {
-      return node;
-    }
-    // if not and data is smaller than the root => move left to check smaller values
-    else if (data < node.data) {
-      return this._searchNode(node.left, data);
-    }
-    // if not and data isn't smaller than the root => move right to check greater values
-    else {
-      return this._searchNode(node.right, data);
+    else if (data <= parentNode.data) {
+        if (parentNode.left) {
+            this.findNodeToInsert(data, parentNode.left);
+            return;
+        }
+        parentNode.left = new Node(data);
     }
   }
 
-  remove(data) {
-    this._root = this._removeNode(this._root, data);
+  has( data ) {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if(!this._root) return false;
+    let currentNode = this._root;
+    return findNode(data, currentNode);
+    function findNode(data, current){
+    if (current?.data === data) {
+      return true;
+    }
+    if(data > current?.data)
+      return findNode(data, current?.right);
+    else if(data < current?.data)
+      return findNode(data, current?.left);
+    else
+      return false;
+   }
   }
 
-  _removeNode(node, data) {
-    // base case => we haven't found the node
-    if (!node) {
-      return null;
-    }
+  find( data ) {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if(!this._root) return null
+    let currentNode = this._root;
 
-    if (data < node.data) {
-      // if not and data is smaller => move to the left
-      node.left = this._removeNode(node.left, data);
-    } // if not and data is bigger => move to the right
-    else if (data > node.data) {
-      node.right = this._removeNode(node.right, data);
+    /*if (currentNode.data === data) {
+      return currentNode;
+    }*/
+    return findNode(data, currentNode);
+
+    /*if (data > currentNode?.data) {
+        this.contains(data, currentNode?.right);
+    } else if (data < currentNode?.data) {
+        this.contains(data, currentNode?.left)
     } else {
-      // of one child
-      if (!node.left) {
-        // no left child => return right or null (if no right too)
-        return node.right;
-      } // no right child => return left
-      else if (!node.right) {
-        return node.left;
-      }
-
-      // if two children => compare
-      // find min in the right => successor
-
-      // replace current node with successor
-      node.data = this._findMinValue(node.right);
-
-      // recursion once again to remove successor
-      node.right = this._removeNode(node.right, node.data);
+        console.log("Node dosen't contain to this tree");
+        return null;
+    }*/
+   function findNode(data, current){
+    if (current?.data === data) {
+      return current;
     }
-
-    return node;
-  }
-
-  _findMinValue(node) {
-    let current = node;
-    // move to the most left leaf (the smallest value)
-    while (current.left) {
-      current = current.left;
-    }
-    return current.data;
-  }
-
-  _findMaxValue(node) {
-    let current = node;
-    // move to the most right leaf (the greatest value)
-    while (current.right) {
-      current = current.right;
-    }
-    return current.data;
-  }
-
-  min() {
-    // if empty
-    if (!this._root) {
+    if(data > current?.data)
+      return findNode(data, current?.right);
+    else if(data < current?.data)
+      return findNode(data, current?.left);
+    else
       return null;
+   }
+  }
+
+  remove( value ) {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here    
+    this._root = removeNode(this._root, value);
+    function removeNode(current, value) {
+      if (value == null || value == undefined)
+          return;
+
+      if (value == current.data) {
+          if (current.left == null && current.right == null) {
+              return null;
+          } else if (current.left == null)
+              return current.right;
+          else if (current.right == null)
+              return current.left;
+          else {
+              var tempNode = kthSmallestNode(current.right);
+              current.data = tempNode.data;
+              current.right = removeNode(current.right, tempNode.data);
+              return current;
+          }
+
+
+      } else if (value < current.data) {
+          current.left = removeNode(current.left, value);
+          return current;
+      } else {
+          current.right = removeNode(current.right, value);
+          return current;
+      }
     }
 
-    return this._findMinValue(this._root);
+    function kthSmallestNode(node) {
+        while (!(node.left == null))
+            node = node.left;
+
+        return node;
+    }
+
+
+  }
+  
+  
+  min() {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if(!this._root)
+      return null;
+    let currentNode = this._root;
+    return findNode(currentNode);
+
+    function findNode(current) {
+      if(!current.left)
+        return current.data;
+      return findNode(current.left);
+    }
+
   }
 
   max() {
-    // if empty
-    if (!this._root) {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if(!this._root)
       return null;
-    }
+    let currentNode = this._root;
+    return findNode(currentNode);
 
-    return this._findMaxValue(this._root);
+    function findNode(current) {
+      if(!current.right)
+        return current.data;
+      return findNode(current.right);
+    }
   }
 }
 
 module.exports = {
-  BinarySearchTree,
+  BinarySearchTree
 };
